3ef11e879c7e7aa54589155bbeea483957f0265e
Ana Júlia Júlia <85965741+AnaJuliaVancan@users.noreply.github.com>
2022-05-04 21:01:08 +0000

    Atualizar e Deletar criados.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", function(req, res){
+app.get("/", async function(req, res){
   var aparece = await usuario.findAll();
   res.json(aparece)
 });
@@ -15,7 +15,15 @@ app.post("/", function(req, res){
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
+app.put("/:id", function(req, res){
+  var atualizando = usuario.update(req.body, {where: {id: req.params.id} });
+  res.json(atualizando);
+});
+app.delete("/:id", function(req, res){
+  var deletando = usuario.destroy({where: {id: req.params.id} });
+  res.json(deletando);
+});
 
 app.listen(3000, function(){
-  console.log("Tudo ")
+  console.log("Tudo certo")
 })
