380e5789f4c7fd7d6091599e4e29797ff6ba06a6
Ana Júlia Júlia <85965741+AnaJuliaVancan@users.noreply.github.com>
2022-05-11 21:22:44 +0000

    Tabelas atualizadas e relacionamentos criados.

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 var express = require("express");
 var app = express();
 var { usuario } = require("./models");
+var { empresa } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res){
+/* Usuários */
+app.get("/usuarios", async function(req, res){
   var aparece = await usuario.findAll();
   res.json(aparece)
 });
-app.post("/", function(req, res){
+app.post("/usuarios", function(req, res){
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
-app.put("/:id", function(req, res){
+app.put("/usuarios/:id", function(req, res){
   var atualizando = usuario.update(req.body, {where: {id: req.params.id} });
   res.json(atualizando);
 });
-app.delete("/:id", function(req, res){
+app.delete("/usuarios/:id", function(req, res){
   var deletando = usuario.destroy({where: {id: req.params.id} });
+  res.json(deletando);
+});
+
+/* Empresas */
+app.get("/empresas", async function(req, res){
+  var aparece = await empresa.findAll();
+  res.json(aparece)
+});
+app.post("/empresas", function(req, res){
+  var resultado = empresa.create(req.body);
+  res.json(resultado);
+});
+app.put("/empresas/:id", function(req, res){
+  var atualizando = empresa.update(req.body, {where: {id: req.params.id} });
+  res.json(atualizando);
+});
+app.delete("/empresas/:id", function(req, res){
+  var deletando = empresa.destroy({where: {id: req.params.id} });
   res.json(deletando);
 });
 
